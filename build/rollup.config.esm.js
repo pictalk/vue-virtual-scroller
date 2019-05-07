0aa5011f650a9c296392996bc6a439c2da8c7abb
Alex Regan <alex.joseph.regan@gmail.com>
2019-05-06 19:44:49 -0700

    fix: merge rollup externals

--- build/rollup.config.esm.js
+++ build/rollup.config.esm.js
@@ -6,7 +6,12 @@ const config = Object.assign({}, base, {
     file: "dist/vue-virtual-scroller.esm.js",
     format: "es",
   },
-  external: ["scrollparent", "vue-observe-visibility", "vue-resize"],
+  external: [
+    ...base.external,
+    "scrollparent",
+    "vue-observe-visibility",
+    "vue-resize",
+  ],
 });
 
 export default config;
